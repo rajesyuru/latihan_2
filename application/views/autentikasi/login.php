932f9d08852520abbb008688bfdfa81760c46b32
sherl <sherlockeria@gmail.com>
2021-12-27 09:55:48 +0700

    pustaka booking

---
+++ application/views/autentikasi/login.php
@@ -0,0 +1,49 @@
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
+                            <div class="col-lg">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                                    </div>
+                                    <?= $this->session->flashdata('pesan'); ?>
+                                    <form class="user" method="POST" action="<?= base_url('autentikasi'); ?>">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user" value="<?= set_value('email'); ?>" id="email" aria-describedby="emailHelp" placeholder="Email" name="email">
+                                            <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user" id="password" placeholder="Password" name="password">
+                                            <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
+                                        </div>
+                                        <button class="btn btn-primary btn-user btn-block" name="submit" type="submit">Login</button>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="<?= base_url('autentikasi/lupaPassword'); ?>">Lupa Password</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="<?= base_url('autentikasi/register'); ?>">Buat Akun Baru</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
---
+++ application/views/v_about.php
@@ -0,0 +1,4 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p>Pariatur laborum ullamco ad voluptate quis nulla ipsum nisi eiusmod. Ex Lorem anim pariatur labore adipisicing Lorem ullamco ullamco reprehenderit cupidatat mollit velit nisi consequat. Pariatur sint occaecat aliqua enim ad. Cupidatat sunt sint nostrud occaecat in consectetur.</p>
+</section>
